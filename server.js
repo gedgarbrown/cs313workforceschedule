bc8551e78109417d6103ed0ae10bae033d65e139
GeorgeCavaco <georgecavaquinho@hotmail.com>
2019-11-30 22:31:54 -0700

    new schedule, link to database

--- server.js
+++ server.js
@@ -20,59 +20,120 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/employees',function(req,res){
-	//var sql = "SELECT * FROM employees";
-	
-
-	
-	employees_json = getEmployeesFromDb(function(err, res) {
-		
-		if(err || res == null) {
-			console.log("Error");
-		}
-	
-	});
-	
-	console.log(employees_json);
-	
-	const response = "Employees: " + employees_json;
-	//console.log(response);
-	//res.send(employees_json);
-	res.render('employees', {data: {response: response}});	
-		
-	res.end();
-});
+app.post('/employees',getEmployeesFromDb, showEmployees);
+app.post('/schedule_date', getHoursByDate, showHoursByDate);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-function getEmployeesFromDb(callback) {
+function findFirstAndLastName(result) {
+	var response = "Employees: ";
+	for (var i = 0; i < result.length; i++) {
+			firstname = result[i].name_first;
+			lastname =  result[i].name_last;
+			skills = " -- Skills: ";
+			if (result[i].server == 't')
+				skills += "Server; ";
+			if (result[i].host == 't')
+				skills += "Host; ";
+			if (result[i].kitchen == 't')
+				skills += "Kitchen; ";
+			if (result[i].cleanup == 't')
+				skills += "Cleanup; ";
+			if (result[i].Manager == 't')
+				skills += "Manager; ";
+			
+			
+			response += "<br>" + firstname + " " + lastname + " " + skills;
+			console.log("working:");
+		}	
+	return response;
+};
+
+function showHoursByDate(req, res) {
+		date = req.body.date;
+		console.log(date);
+		response = JSON.stringify(req.hour);
+		
+		res.render('schedule_date', {data: {response: response, date: date}});	
+	};
+	
+function showEmployees(req, res) {
+		response = findFirstAndLastName(req.employees);
+		//date = JSON.stringify(req.body.date);
+		//console.log(date);
+		//response = req.employees;
+		res.render('employees', {data: {response: response}});	
+	};
+	
+function getHoursByDate(req, res, next) {
+	date = req.body.date;
+	var sql = "SELECT * FROM hour WHERE date ='" + date +"'";
+	console.log(date);
+	console.log(sql);
+	
+	//var response;
+	
+	pool.query(sql, function(err, result) {
+		if(err) {
+			console.log("Error in query: ");
+			console.log(err);
+			//callback(err, null);
+			return null;
+		} else {
+			
+		//console.log(1);	
+		
+		/*for (var i = 0; i < 1; i++) {
+			firstname = JSON.stringify(result.rows[i].name_first);
+			lastname =  JSON.stringify(result.rows[i].name_last);
+			response += "\n" + firstname + " " + lastname;
+			console.log("working:");
+		}*/			
+		
+		console.log("Back from DB with result:");
+		console.log(result.rows);
+		//console.log(response);
+		req.hour= result.rows;
+		next();
+		}
+		
+		
+	});
+	
+}//end getHoursByDate
+
+function getEmployeesFromDb(req, res, next) {
 	const sql = "SELECT * FROM employees";
 	
-	var response = "Employees: ";
+	//var response;
 	
 	pool.query(sql, function(err, result) {
 		if(err) {
 			console.log("Error in query: ");
 			conole.log(err);
-			callback(err. null);
-		}
-	
-		console.log("Back from DB with result:");
-		console.log(result.rows);
+			//callback(err, null);
+			return null;
+		} else {
+			
+		//console.log(1);	
 		
-		
-		
-		for (var i = 0; i <= result.rows.legnth; i++) {
+		/*for (var i = 0; i < 1; i++) {
 			firstname = JSON.stringify(result.rows[i].name_first);
 			lastname =  JSON.stringify(result.rows[i].name_last);
 			response += "\n" + firstname + " " + lastname;
-			console.log(response);
-		}			
+			console.log("working:");
+		}*/			
 		
-		console.log(response);
+		console.log("Back from DB with result:");
+		console.log(result.rows);
+		//console.log(response);
+		req.employees= result.rows;
+		next();
+		}
 		
 		
 	});
+
+
 	
-	callback(response);
 }//end of getEmployeesFromDb
